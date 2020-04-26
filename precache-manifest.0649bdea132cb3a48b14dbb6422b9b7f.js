ac38a13c039c35178bb00aba4d2f58f0a007edfd
Firmansyah <fsyah7052@gmail.com>
2020-04-26 12:12:49 +0700

    deploy

---
+++ precache-manifest.0649bdea132cb3a48b14dbb6422b9b7f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155d5232beaa3fcd9510",
+    "url": "porfolio/css/app.94f465be.css"
+  },
+  {
+    "revision": "615448881e05e1e2a008",
+    "url": "porfolio/css/chunk-vendors.51c0f58e.css"
+  },
+  {
+    "revision": "ba7b72805aa413f51d8928154e222274",
+    "url": "porfolio/img/bootstrap-logo.ba7b7280.svg"
+  },
+  {
+    "revision": "e0f59278fd30b3647563a2a7af4d5edc",
+    "url": "porfolio/img/devlearn.e0f59278.png"
+  },
+  {
+    "revision": "5df43f0acb9d911c2b245bb75b8ac02b",
+    "url": "porfolio/img/frontend.5df43f0a.png"
+  },
+  {
+    "revision": "ae891b579fd9569826fee0d72590503a",
+    "url": "porfolio/img/jquery-logo.ae891b57.svg"
+  },
+  {
+    "revision": "180042b8c9a28ffa5329f85a51faee0d",
+    "url": "porfolio/img/list-icon.180042b8.svg"
+  },
+  {
+    "revision": "105ba09e9afe0ced3485d2f64ca357cd",
+    "url": "porfolio/img/me.105ba09e.png"
+  },
+  {
+    "revision": "4c01caf0e86f8161df0205ac67166345",
+    "url": "porfolio/img/me2.4c01caf0.png"
+  },
+  {
+    "revision": "ec643e62e8e8d65483d259878eec37c8",
+    "url": "porfolio/img/organization.ec643e62.png"
+  },
+  {
+    "revision": "eed0f39a99ffe24391438c1cbac4abef",
+    "url": "porfolio/img/pemro.eed0f39a.png"
+  },
+  {
+    "revision": "38a16993541fb45db9a9d2a476a4be2a",
+    "url": "porfolio/img/portfolio.38a16993.png"
+  },
+  {
+    "revision": "12f36ece7b95e1d9252a4516e2a26f5b",
+    "url": "porfolio/img/sass-logo.12f36ece.svg"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "porfolio/img/tailwind-logo.760a3ee4.svg"
+  },
+  {
+    "revision": "8e87da8a488b7a3f4a8a7213ca7a8c5c",
+    "url": "porfolio/img/uiux.8e87da8a.png"
+  },
+  {
+    "revision": "7271957151cda0b57d2ceb56baa3a053",
+    "url": "porfolio/img/vue-logo.72719571.svg"
+  },
+  {
+    "revision": "456f6cfe3adc8e4c06ee586d6224e2e4",
+    "url": "porfolio/img/webpack-logo.456f6cfe.svg"
+  },
+  {
+    "revision": "a4f79a2023b7baa0643b07b9a61bd28a",
+    "url": "porfolio/index.html"
+  },
+  {
+    "revision": "155d5232beaa3fcd9510",
+    "url": "porfolio/js/app.b0e029ce.js"
+  },
+  {
+    "revision": "615448881e05e1e2a008",
+    "url": "porfolio/js/chunk-vendors.a2049858.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "porfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "porfolio/robots.txt"
+  }
+]);
