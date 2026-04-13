401027f187f7e5f921279cc7fa02c1c500322bb0
Rahmad Firmansyah <rahmadfirmansyah.id@gmail.com>
2026-04-13 11:00:29 +0700

    chore: add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,20 @@ const newsreader = Newsreader({
 export const metadata: Metadata = {
   title: "Rahmad Firmansyah | Software Engineer",
   description: "As Software Engineer he is either a magician or a someone who can turn coffee and late nights into interactive and high-performance apps that make your product running smoothly and your users engaged",
+  icons: {
+    icon: [
+      { url: "/favicons/favicon.ico" },
+      { url: "/favicons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/favicons/apple-touch-icon.png" },
+    ],
+    other: [
+      { rel: "android-chrome-192x192", url: "/favicons/android-chrome-192x192.png" },
+      { rel: "android-chrome-512x512", url: "/favicons/android-chrome-512x512.png" },
+    ],
+  },
 };
 
 export default function RootLayout({
